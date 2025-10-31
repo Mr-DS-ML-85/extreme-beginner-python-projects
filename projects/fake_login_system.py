55bc375204254a64115730774536630bca0e463c
Mr.ML Learner <gojmanday@gmail.com>
2025-10-31 13:08:24 +0600

    Enhance login system with user and admin support
    Refactor login system to support multiple users and admins with password validation. Implement a maximum of three login attempts and handle KeyboardInterrupt for graceful exit.

--- projects/fake_login_system.py
+++ projects/fake_login_system.py
@@ -1,20 +1,38 @@
 system = True
-while system :
-    x = input("Enter Your Username = ")
-    y = input("Enter Your Password = ")
-    if x == "Douglas":
-       print("Username Valid")
-       if y == "abc123!@#" :
-           print("Login Successfully\nWelcome David Douglas(@admin)")
-           system = False
-       else :
-            print("Invalid Password")
-    elif x == "imjohnny":
+count = 0
+try:
+  while system and count < 3:
+      x = input("Enter Your Username = ")
+      y = input("Enter Your Password = ")
+      users = ["Alice","Bob","David Joe","Darwin", "imjohnny"]
+      admins = ["Douglas", "Joel", "Sarah"]
+      admins_pass = {"Douglas": "abc123!@#",
+                   "Joel": "joel2024$%",
+                   "Sarah": "sarahSecure99"
+                  }
+      users_pass = {"imjohnny": "jangoboy86",
+                    "Alice": "alicePass1",
+                   "Bob": "bobSecure2",
+                   "David Joe": "davidJ2024",
+                   "Darwin": "darwinX99"
+                   }
+      if x in admins and y == admins_pass[x]:
+       print("Username Valid") 
+       print(f"Login Successfully\nWelcome {x}(@admin)")
+       system = False
+      elif x in users and y == users_pass[x]:
          print("Username Valid")
-         if y == "jangoboy86":
-             print("Login Successfully\nWelcome Johnny Davy(@user)")
-             system = False
-         else :
-              print("Invalid Password")
-    else:
-       print("Login Failed \nInvalid Username")
+         print(f"Login Successfully\nWelcome {x}(@user)")
+         system = False
+      else:
+               print("Invalid Username or Password")
+               print("Login Failed \n Try Again")
+               count += 1
+      if count == 3:
+          print("Maximum login attempts reached. Exiting.")
+          system = False
+except KeyboardInterrupt:
+    m = input("\n\nDo you want to exit the login page?(y/n):")
+    z = m.lower()
+    if z == 'y':
+        system = False
